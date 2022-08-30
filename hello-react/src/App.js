eb663423728c9c065e1250d756746412313d8e06
Nasanbat G <nasanbatg@Nasanbats-MacBook-Pro.local>
2022-08-30 10:26:41 +0800

    b1-day4

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,17 +1,15 @@
 import React from "react";
-import { BrowserRouter, Routes, Route   } from "react-router-dom";
-import { Contact, Header, LogIn, Products ,BlogDetail, Blogs} from "./components";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Contact, LogIn, Home, BlogDetail, Blogs, Header } from "./components";
 
 const App = () => {
-
- 
   return (
     <div className="container">
       <BrowserRouter>
+          <Header />
         <Routes>
-          <Route path="/" exact element={<Header />} />
+          <Route path="/" exact element={<Home />} />
           <Route path="/Blog" element={<Blogs />} />
-          <Route path="/Products" element={<Products />} />
           <Route path="/Contact" element={<Contact />} />
           <Route path="/LogIn" element={<LogIn />} />
           <Route path="/Blog/:id" element={<BlogDetail />} />
--- hello-react/src/components/Blog/Blogs.jsx
+++ hello-react/src/components/Blog/Blogs.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import { BlogPost, Header } from "../index";
-import { MockData } from "../MOCK_DATA";
+import { BlogPost } from "../index";
+import MockData from "../MOCK_DATA.json";
 
 export const Blogs = () => {
   return (
     <div className="d-flex flex-row gap-5 row justify-content-center ">
-      <Header />
       {MockData.map((el, index) => {
         return <BlogPost child={el} className="col-4" index={index} />;
       })}
